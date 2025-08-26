9fb663a9f0cda304dd65be5f4b1d190e0d40616f
meAltf <alataf.dce.cs1@gmail.com>
2025-08-26 23:40:45 +0530

    if-else with user input as integer

--- if-else/main.go
+++ if-else/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	x := 21
@@ -11,4 +17,27 @@ func main() {
 	} else {
 		fmt.Println("x is smaler than 5")
 	}
+
+	fmt.Println("____________________________________________________________________________________")
+	fmt.Println("Take a inpput from user")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Enter a number")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input) // remove newline
+
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Invalid num, please enter a valid integer")
+		return
+	}
+
+	if num > 100 {
+		fmt.Println("Number is greater than 100")
+	} else if num == 100 {
+		fmt.Println("Number is eaxactly equal to 100")
+	} else {
+		fmt.Println("Number is less than 100")
+	}
+
 }
