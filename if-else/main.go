0b8cb1d4d0ccefa4b972e9af4c18405d4d8baf86
meAltf <alataf.dce.cs1@gmail.com>
2025-08-26 23:10:29 +0530

    if-else | basic

---
+++ if-else/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 21
+	if x > 5 {
+		fmt.Println("x is greate than")
+	} else if x == 15 {
+		fmt.Println("x is equal to 15")
+	} else {
+		fmt.Println("x is smaler than 5")
+	}
+}
